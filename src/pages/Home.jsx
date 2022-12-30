5947712157d7e22aa3d3700372f949990c8916d6
theo-lemoine-oc <propjhh>
2022-12-30 02:29:45 +0100

    Feature/Add responsive on banner hero homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,16 +7,16 @@ import BannerImg from '../assets/img/footstats-hero.png';
 function Home() {
   return (
     <section className='bg-gray-200'>
-      <div className='flex py-8 md:py-0 max-w-[80%] mx-auto'>
-        <div className='flex flex-col items-center justify-center w-1/2 px-8'>
+      <div className='flex flex-col tablet:flex-row pt-8 md:py-0 max-w-[80%] mx-auto'>
+        <div className='flex flex-col justify-center text-center tablet:w-1/2 tablet:text-left'>
           <MainTitle content="Gardez l'œil sur le football du moment" />
           <Paragraph content=
             "Retrouvez toutes les statistiques du football en direct, en allant des joueurs aux clubs en passant par les ligues et les grands championnats."
           />
         </div>
 
-        <div className='w-1/2 flex justify-center items-center'>
-          <img src={ BannerImg } alt="Cristiano Ronaldo, Kylian Mbappé, Lionel Messi" className='w-[500px]' />
+        <div className='tablet:w-1/2 flex justify-center items-center'>
+          <img src={ BannerImg } alt="Cristiano Ronaldo, Kylian Mbappé, Lionel Messi" className='w-[350px] tablet:w-[500px]' />
         </div>
       </div>
     </section>
