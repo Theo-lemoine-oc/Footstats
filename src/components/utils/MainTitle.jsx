4ee3ab62c79e840f53880362c78ce95e1d782e28
theo-lemoine-oc <propjhh>
2022-12-30 02:17:44 +0100

    Feature/Hompage

--- src/components/utils/MainTitle.jsx
+++ src/components/utils/MainTitle.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const MainTitle = (props) => {
     return (
-        <h1 className='font-bold text-4xl'>{ props.content }</h1>
+        <h1 className='font-bold text-4xl mb-4'>{ props.content }</h1>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { MainTitle } from '../components/utils/MainTitle';
+import { Paragraph } from '../components/utils/Paragraph';
+import BannerImg from '../assets/img/footstats-hero.png';
+
 
 function Home() {
   return (
-    <div>
-      <MainTitle content="Page d'accueil" />
-    </div>
+    <section className='bg-gray-200'>
+      <div className='flex py-8 md:py-0 max-w-[80%] mx-auto'>
+        <div className='flex flex-col items-center justify-center w-1/2 px-8'>
+          <MainTitle content="Gardez l'œil sur le football du moment" />
+          <Paragraph content=
+            "Retrouvez toutes les statistiques du football en direct, en allant des joueurs aux clubs en passant par les ligues et les grands championnats."
+          />
+        </div>
+
+        <div className='w-1/2 flex justify-center items-center'>
+          <img src={ BannerImg } alt="Cristiano Ronaldo, Kylian Mbappé, Lionel Messi" className='w-[500px]' />
+        </div>
+      </div>
+    </section>
   );
 }
 
